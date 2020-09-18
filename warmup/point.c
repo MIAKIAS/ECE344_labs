153a416665c2fc272fa0607cd57fcbaa79bedfe8
Weizhou Wang <weizhou.wang@mail.utoronto.ca>
2020-09-18 05:47:55 -0400

    Committing changes for Lab 1

--- warmup/point.c
+++ warmup/point.c
@@ -1,23 +1,30 @@
 #include <assert.h>
+#include <math.h>
 #include "common.h"
 #include "point.h"
 
 void
 point_translate(struct point *p, double x, double y)
 {
-	TBD();
+	p->x += x;
+	p->y += y;
 }
 
 double
 point_distance(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return -1.0;
+	double sideA = p2->y - p1->y;
+	double sideB = p2->x - p1->x;
+	return sqrt(pow(sideA, 2) + pow(sideB, 2));
 }
 
 int
 point_compare(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return 0;
+	struct point origin;
+	origin.x = origin.y = 0;
+	double dif = point_distance(p1, &origin) - point_distance(p2, &origin);
+	if (dif < 0) return -1;
+	else if (dif == 0) return 0;
+	else return 1;
 }
